8f3fceb12c87daf99aa51588abb9d1269db04ef0
Triomph <worker13prime@gmail.com>
2024-11-20 11:21:18 +0100

    Add get_project() method

--- include/ProjectRepository.hpp
+++ include/ProjectRepository.hpp
@@ -4,7 +4,7 @@
 #include "User.hpp"
 #include <string>
 #include <vector>
-
+#include <optional>
 
 
 class ProjectRepository 
@@ -12,6 +12,7 @@ class ProjectRepository
 public:
     // CRUD operations
     static void create_project(const std::string& name, const std::string& description, const std::string& start_date, const std::string& end_date);
+    static std::optional<Project> get_project(int id);
     static std::vector<Project> get_projects();
     static void update_project(int id, const std::string& name, const std::string& description, const std::string& start_date, const std::string& end_date);
     static void delete_project(int id);
